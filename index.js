2b6920f8c95da33739dd580f5e611c657e633790
rumbleh <rumbleh@gmail.com>
2021-06-07 23:08:49 -0300

    Teste

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('hello world!'))
 
-app.listen(3000, () => console.log('Servidor escutando na porta 3000'))
+app.listen(3000, () => console.log('Servidor escutando na porta 3000')) 
