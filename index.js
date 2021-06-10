e5844bd70208c608cc46c35c6dced301aa364842
rumbleh <rumbleh@gmail.com>
2021-06-10 20:13:59 -0300

    Mudanças para mongodb, controllers e Swagger

--- index.js
+++ index.js
@@ -1,50 +1,14 @@
 const express = require('express')
 const app = express()
-const db = require('./db')
+const rotas = require('./routes')
 require('dotenv/config');
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.get('/created_at', async function (req, res) {  
-  
-  result = await db.buscaPorData(req.query.data)
-  if (result.length === 0){
-    res.status(404).end()
-  }
-  res.json(result)      
-
-})
-
-app.get('/:shorturl', async function (req, res) {  
-  
-  result = await db.buscaPorShorturl(req.params.shorturl)
-  
-  if (result.length === 0){
-    res.status(404).end()
-  }
-  saida = await db.incrementaHit(result[0].id)
-  
-  res.json({longurl: result[0].longurl})      
-
-})
-
-app.get('/id/:id', async function (req, res) {  
-  
-  result = await db.buscaPorId(req.params.id)
-  if (result.length === 0){
-    res.status(404).end()
-  }
-    
-  res.json(result[0])      
-
-})
-
-app.post('/create', async function (req, res)  {    
-
-  result = await db.encurtar(req.body.longurl)
-  res.json(result)      
-
-})
+require('./routes')(app)
 
 app.listen(3000, () => console.log('Servidor escutando na porta 3000')) 
-
